5b8d2eff4079a735b780772f81732e11208c32d3
Camilo Arias Guerrero <camiloalex2000@gmail.com>
2024-06-27 11:26:38 -0500

    se movio creacion_tabla_actualizada a operaciones_bdoracle.py

---
+++ scripts/actualizar_compensados.py
@@ -0,0 +1,77 @@
+from tkinter import filedialog
+from sqlalchemy import create_engine
+import pathlib
+import pandas as pd
+from operaciones_bdoracle import conectar_base_oracle, creacion_tabla_actualizada
+
+# Constantes
+# Archivos permitidos para cada grupo
+beneficiario: list[str] = ['CORRECCION_BEN', 'AB4023']
+cotizante: list[str] = ['CORRECCION_COT', 'AC4023']
+liquidado: list[str] = ['EPSS05RESTITUCION', 'EPSS05LIQUIDACION']
+# Objeto con nombre de tablas
+tablas: dict[str, str] = {
+    'beneficiario': 'TBL_OPE_COMPENSADO_BEN',
+    'cotizante': 'TBL_OPE_COMPENSADO_COT',
+    'liquidado': 'EPSS05LIQUIDACION'
+}
+
+
+def actualizar_compensados(engine: create_engine, ruta_compensados: pathlib.Path) -> None:
+    # Preguntar periodo de actualizacion
+    periodo = input("Ingrese el periodo de actualización (YYYYMM): ")
+
+    archivos = [archivo for archivo in ruta_compensados.iterdir(
+    ) if archivo.is_file() and archivo.suffix == '.txt']
+
+    df_liquidados: list[pd.DataFrame] = []
+
+    for archivo in archivos:
+        if liquidado[0] in archivo.name or liquidado[1] in archivo.name:
+            df: pd.DataFrame = pd.read_csv(
+                archivo, sep=',', dtype='str', encoding='latin1')
+            df.drop(columns=['CAUSAL_DE_RESTITUCION',
+                    'TIPO DE NOVEDAD'], inplace=True, errors='ignore')
+            df_liquidados.append(df)
+
+    df_liquidado = pd.concat(df_liquidados)
+
+    creacion_tabla_actualizada(engine, df_liquidado, tablas['liquidado'], periodo)
+
+    df_cotizantes: list[pd.DataFrame] = []
+
+    for archivo in archivos:
+        if cotizante[0] in archivo.name or cotizante[1] in archivo.name:
+            df = pd.read_csv(archivo, sep=',', dtype='str', encoding='latin1')
+            df.rename(columns={'DEPARTAMENTO': 'COD_DEP', 'MUNICIPIO': 'COD_MUN',
+                      'EXONERACIÓN': 'EXONERACION', 'CENTRO_COSTO': 'CENTRO_DE_COSTO'}, inplace=True)
+            df_cotizantes.append(df)
+
+    df_cotizante = pd.concat(df_cotizantes)
+
+    creacion_tabla_actualizada(engine, df_cotizante, tablas['cotizante'], periodo)
+
+    df_beneficiarios: list[pd.DataFrame] = []
+
+    for archivo in archivos:
+        if beneficiario[0] in archivo.name or beneficiario[1] in archivo.name:
+            df = pd.read_csv(archivo, sep=',', dtype='str', encoding='latin1')
+            df.rename(columns={'DEPARTAMENTO': 'COD_DEP', 'MUNICIPIO': 'COD_MUN',
+                      'EXONERACIÓN': 'EXONERACION'}, inplace=True)
+            df_beneficiarios.append(df)
+
+    df_beneficiario = pd.concat(df_beneficiarios)
+
+    creacion_tabla_actualizada(
+        engine, df_beneficiario, tablas['beneficiario'], periodo)
+
+
+
+
+if __name__ == '__main__':
+    ruta_compensados = pathlib.Path(filedialog.askdirectory(
+        initialdir=r'G:\.shortcut-targets-by-id\1buUUJ2naBFTn-E10CXNd8elJ6YQOlWCR\00_BASES_COMPENSACION_2024'))
+
+    engine: create_engine = conectar_base_oracle()
+
+    actualizar_compensados(engine, ruta_compensados)
--- scripts/operaciones_bdoracle.py
+++ scripts/operaciones_bdoracle.py
@@ -5,7 +5,7 @@ import configparser
 import pandas as pd
 
 # Constantes
-CONFIG_PATH = '../config.ini'
+CONFIG_PATH = 'config.ini'
 
 
 def leer_configuracion_oracle():
@@ -24,7 +24,8 @@ def conectar_base_oracle() -> create_engine:
     password: str = config['oracle']['password']
 
     dsn: str = cx_Oracle.makedsn(host, port, sid)
-    engine: create_engine = create_engine(f'oracle+cx_oracle://{user}:{password}@{dsn}')
+    engine: create_engine = create_engine(
+        f'oracle+cx_oracle://{user}:{password}@{dsn}')
     return engine
 
 
@@ -37,4 +38,39 @@ def actualizar_datos_oracle(engine: create_engine, df: pd.DataFrame, tabla: str)
             print(e)
         df.to_sql(tabla, con=engine, if_exists='append', index=False)
         cursor.close()
-    print(f'Tabla {tabla} actualizada')
+    print(f'Se actualizaron {df.shape[0]} registros en la tabla {tabla}')
+
+
+def creacion_tabla_actualizada(engine: create_engine, df: pd.DataFrame, tabla: str, periodo: str) -> None:
+
+    periodo_anterior = str(int(periodo) - 1)
+
+    with engine.connect() as connection:
+        # Crear cursor
+        cursor = connection.connection.cursor()
+
+        # Eliminar tabla si existe
+        try:
+            cursor.execute(f"""
+                DROP TABLE {tabla.lower()}_{periodo}
+            """)
+        except Exception as e:
+            print(e)
+
+        # Crear copia de la tabla del periodo anterior
+        try:
+            cursor.execute(f"""
+                CREATE TABLE {tabla.lower()}_{periodo} AS
+                SELECT * FROM {tabla.lower()}_{periodo_anterior} WHERE 1=0
+            """)
+        except Exception as e:
+            print(e)
+
+    # Hacer la insercion por chunk df
+    chunksize = 50000
+    for i in range(0, df.shape[0], chunksize):
+        df.iloc[i:i+chunksize].to_sql(tabla.lower() +
+                                      f'_{periodo}', engine, if_exists='append', index=False)
+
+    print(f'''Tabla {tabla.lower()}_{
+          periodo} creada con éxito, se insertaron {df.shape[0]} registros''')
