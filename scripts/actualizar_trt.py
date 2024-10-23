35304c4b1eab7af86625b813332210eda1fc0ecd
Camilo Arias Guerrero <camiloalex2000@gmail.com>
2024-10-22 19:20:47 -0500

    Agregar actualizacion TRT medicamentos

---
+++ scripts/actualizar_trt.py
@@ -0,0 +1,43 @@
+import logging
+from pathlib import Path
+from unidecode import unidecode
+import pandas as pd
+from tkinter import filedialog
+from database.operaciones_bdoracle import conectar_base_oracle, actualizar_datos_oracle, crear_tabla_bytes
+from scripts.limpieza_archivo import sacar_longitudes_max_columnas, convertir_texto_dataframe, limpiar_texto_columnas
+
+NOMBRE_TABLA_TRT_ORACLE = 'TBL_OPE_NT_TRT_MEDICAMENTOS_'
+
+def actualizar_trt_medicamentos(engine, ruta_trt: Path, nombre_tabla: str) -> None:
+    logging.info(f'Actualizando TRT Medicamentos {ruta_trt.name}')
+    
+    df_columnas = pd.read_excel(ruta_trt, dtype='str', nrows=0)
+    
+    columnas = limpiar_texto_columnas(df_columnas)
+        
+    df = pd.read_excel(ruta_trt, dtype='str', names=columnas)
+    
+    df.columns = df.columns.str.replace('.', '')
+    
+    df = df.pipe(convertir_texto_dataframe)
+
+    longitudes_max_columnas = sacar_longitudes_max_columnas(df)
+    
+    crear_tabla_bytes(engine ,nombre_tabla, df, df.columns, longitudes_max_columnas)
+    
+    actualizar_datos_oracle(engine, df, nombre_tabla)
+
+def quitar_acentos(texto: str) -> str:
+    return unidecode(texto)
+
+
+if __name__ == '__main__':
+    ruta_trt = Path(filedialog.askopenfilename())
+    
+    version_tabla = input('Ingrese el año y la versión de la tabla TRT (por ejemplo, 2024_41): ')
+    
+    nombre_tabla = f'{NOMBRE_TABLA_TRT_ORACLE}{version_tabla}'.lower()
+    
+    engine = conectar_base_oracle()
+        
+    actualizar_trt_medicamentos(engine, ruta_trt, nombre_tabla)
---
+++ scripts/limpieza_archivo.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from unidecode import unidecode
+
+
+def sacar_longitudes_max_columnas(df: pd.DataFrame) -> dict:
+    return {col: df[col].astype(str).str.len().max() for col in df.columns}
+
+def convertir_texto_dataframe(df:pd.DataFrame) -> pd.DataFrame:
+    for col in df.columns:
+        try:
+            df[col] = df[col].str.strip()
+        except AttributeError:
+            print(f'No se pudo convertir la columna {col} a texto')
+    return df
+            
+def limpiar_texto_columnas(df: pd.DataFrame) -> list[str]:
+    # Reemplazar espacios dobles de las columnas
+    df.columns = df.columns.str.replace(r'\s{2,}', ' ', regex=True).str.strip()
+    
+    df.columns = df.columns.str.replace(' ', '_')
+    
+    # Agregar guion bajo antes de paréntesis de apertura si no está precedido por uno
+    df.columns = df.columns.str.replace(r'([^\_])\(', r'\1_(', regex=True)
+    
+    # Agregar guion bajo antes de paréntesis de cierre si no está precedido por uno y está seguido de un carácter
+    df.columns = df.columns.str.replace(r'([^\_])\)(?=\w)', r'\1_)', regex=True)
+    
+    # Eliminar los paréntesis
+    df.columns = df.columns.str.replace('(', '')
+    df.columns = df.columns.str.replace(')', '')
+    
+    # Quitar acentos de las columnas
+    df.columns = [quitar_acentos(col).upper() for col in df.columns]
+    
+    columnas = df.columns.tolist()
+    
+    return columnas
+
+    
+            
+def quitar_acentos(texto: str) -> str:
+    return unidecode(texto)
